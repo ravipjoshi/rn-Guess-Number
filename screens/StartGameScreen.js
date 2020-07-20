a8990edc7c531e815be16479ae5ee1752e487f45
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-20 12:25:59 -0400

    Added new screen with gamestart screen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+               <Text > {props.title}</Text> 
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+     screen :{
+        flex: 1,
+        alignItems :'center'
+     },
+});
+
+export default StartGameScreen;
