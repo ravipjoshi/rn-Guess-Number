cbcdfc0bb5fa53a825dabeaa46242c88652686fa
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-20 13:39:01 -0400

    Updated Card style in the Startgamescreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,17 +4,17 @@ import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-                
-               <View style={styles.inputContainer} >
-                    <Text > {props.title}</Text>
-                    <TextInput title='Select a number'/>
-                    <View style={styles.buttonContainer}>
-                            <Button title='reset' onPress={()=>{}}/>
-                            <Button title='start' onPress={()=>{}}/>
-                    </View>
+            <Text style={styles.title} > {props.title}</Text>
+            <View style={styles.inputContainer} >
+                <Text> Select a Number</Text>
+                <TextInput title='Select a number'/>
+                <View style={styles.buttonContainer}>
+                    <Button title='reset' onPress={()=>{}}/>
+                    <Button title='start' onPress={()=>{}}/>
+                </View>
                     
 
-               </View>
+            </View>
         </View>
     );
 };
@@ -33,7 +33,18 @@ const styles = StyleSheet.create({
      inputContainer:{
         width: 300,
         maxWidth:'80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor:'black',
+        shadowOpacity:5,
+        shadowOffset:{width:0, height:2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor:'white',
+        elevation: 7,
+        padding :20,
+        borderRadius: 5
+
+
      },
      buttonContainer:{
         flexDirection : 'row',
