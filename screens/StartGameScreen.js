993c10c88e8ab97b19869670dbaa6fcf8565b9c6
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-21 13:03:54 -0400

    Added a usestate and validating entered value if it should not be non number value

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
 import Card from '../components/Card';
 import colors from '../constants/colors';
 import Input from '../components/Input';
 
 const StartGameScreen = props => {
+
+    const [enteredValue,setEnteredValue] = useState('');
+    const numberInputHandler = inputText =>{
+        setEnteredValue(inputText.replace(/[^0-9]/g,''));
+
+    }
+
     return (
         <View style={styles.screen}>
             <Text style={styles.title} > Start a New Game</Text>
             <Card style={styles.inputContainer}>
                 <Text> Select a Number</Text>
-                <Input style={styles.input} keyboardType="numeric" autoCorrect={false} blurOnSubmit autoCapitalize='none' maxLength={2}/>
+                <Input style={styles.input} 
+                    keyboardType="number-pad" 
+                    autoCorrect={false} 
+                    blurOnSubmit 
+                    autoCapitalize='none' 
+                    maxLength={2}
+                    onChangeText={numberInputHandler}
+                    value={enteredValue}/>
                 <View style={styles.buttonContainer} >
                     <View style={styles.button}>
                         <Button title='reset' onPress={()=>{}} color={colors.accent}/> 
